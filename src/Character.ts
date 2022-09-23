e76c36d12eed077ff57b9195ae21a2567b4b21bb
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-09-23 16:09:14 -0300

    Refatorando interface Fighter e conclusão da classe Monster

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -50,8 +50,8 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): number {
-    return enemy.lifePoints - this._strength;
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
   }
 
   levelUp() {
